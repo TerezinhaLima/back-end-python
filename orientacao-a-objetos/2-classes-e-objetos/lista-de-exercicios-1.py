07adb61699a06b20d0358b9dbba0d66eb649d0b2
Terezinha Lima <91030675+TerezinhaLima@users.noreply.github.com>
2024-01-30 13:53:48 -0300

    Update lista-de-exercicios-1.py
    resolução do exercicio 1

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -11,3 +11,49 @@
 
 
 # Faça o carro "parar" utilizando os métodos da sua classe.
+class Carro:
+    def __init__(self, cor, modelo):
+        self.ligado = False
+        self.cor = cor
+        self.modelo = modelo
+        self.velocidade = 0
+
+    def liga(self):
+        if not self.ligado:
+            self.ligado = True
+            print('O carro foi ligado.')
+        else:
+            print('O carro já está ligado.')
+
+    def desliga(self):
+        if self.ligado:
+            self.ligado = False
+            self.velocidade = 0
+            print('O carro foi desligado.')
+        else:
+            print('O carro já está desligado.')
+
+    def acelera(self, quantidade):
+        if self.ligado:
+            self.velocidade += quantidade
+            print(f'O carro acelerou. Velocidade atual: {self.velocidade} km/h')
+        else:
+            print('O carro precisa estar ligado para acelerar.')
+
+    def desacelera(self, quantidade):
+        if self.ligado:
+            if self.velocidade >= quantidade:
+                self.velocidade -= quantidade
+                print(f'O carro desacelerou. Velocidade atual: {self.velocidade} km/h')
+            else:
+                print('O carro já está parado.')
+        else:
+            print('O carro precisa estar ligado para desacelerar.')
+
+# Exemplo de uso da classe Carro
+meu_carro = Carro(cor='Azul', modelo='Sedan')
+
+meu_carro.liga()
+meu_carro.acelera(20)
+meu_carro.desacelera(10)
+meu_carro.desliga()
